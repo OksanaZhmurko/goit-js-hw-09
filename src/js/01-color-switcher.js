1fc43a774babf429edf72d137493fcd76371cf79
OksanaZhmurko <oksanazhmurko@gmail.com>
2023-11-30 20:42:07 +0200

    01-color.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+  }
 
+  const startButton = document.querySelector('[data-start]');
+  const stopButton = document.querySelector('[data-stop]');
+  const body = document.body;
+
+  let intervalId;
+
+  function changeColor() {
+    body.style.backgroundColor = getRandomHexColor();
+  }
+
+  startButton.addEventListener('click', function() {
+    startButton.disabled = true;
+    intervalId = setInterval(changeColor, 1000);
+  });
+
+  stopButton.addEventListener('click', function() {
+    startButton.disabled = false;
+    clearInterval(intervalId);
+  });
+});
