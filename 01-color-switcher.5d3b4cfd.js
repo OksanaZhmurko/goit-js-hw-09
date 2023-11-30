9fb5f70a37c814c4301ea32c350a6990878ab6cf
OksanaZhmurko <97787727+OksanaZhmurko@users.noreply.github.com>
2023-11-30 18:42:43 +0000

    Deploying to gh-pages from @ OksanaZhmurko/goit-js-hw-09@1fc43a774babf429edf72d137493fcd76371cf79 🚀

---
+++ 01-color-switcher.5d3b4cfd.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.body;let d;function o(){n.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`}t.addEventListener("click",(function(){t.disabled=!0,d=setInterval(o,1e3)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(d)}))}));
+//# sourceMappingURL=01-color-switcher.5d3b4cfd.js.map
