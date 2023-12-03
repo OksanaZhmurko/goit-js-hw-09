9fd82727b056e7f152dfb6de611ba54294c29c7c
OksanaZhmurko <oksanazhmurko@gmail.com>
2023-12-03 16:32:58 +0200

    code

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+document.querySelector('.form').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const delay = parseInt(document.querySelector('[name="delay"]').value);
+  const step = parseInt(document.querySelector('[name="step"]').value);
+  const amount = parseInt(document.querySelector('[name="amount"]').value);
+
+  let currentDelay = delay;
+
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, currentDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.failure(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+       
+      });
+
+    currentDelay += step;
+  }
+});
+
+
+
+
+
+
+
